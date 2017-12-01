e27216789cac9e2e90601d428e45f42f6cffd883
rohscx <R_E__g_@Macintosh.local>
2017-12-01 12:36:10 -0500

    setup

--- cmdRunner.h
+++ cmdRunner.h
@@ -4,17 +4,21 @@ char* cmdRunner (char *a) {
 	char buffer[255];
 	char *str_to_ret = malloc (sizeof (char) * 50);
 
-	fp = popen(a, "r");
+	fp = popen(a, "w");
 	if (fp == NULL)
 	    /* Handle error */;
 			printf("error FP Null!");
 
 	while (fgets(buffer, 255, fp) != NULL)
+		 /* Used to trim blank spaces */
 			buffer[strlen(buffer)-1] = '\0';
 			str_to_ret[strlen(buffer)-1] = '\0';
-	    printf("%s", buffer);
+			// debug
+	    //printf("%s", buffer);
+			/* copies buffer to global string */
 			strcpy(str_to_ret,buffer);
-			printf("buffer_out  %s", str_to_ret);
+			// debug
+			//printf("buffer_out  %s", str_to_ret);
 	status = pclose(fp);
 	if (status == -1) {
 	    /* Error reported by pclose() */
